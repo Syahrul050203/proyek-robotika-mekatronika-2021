fa1e4a59eb70a8789712f9ca7c352d7a1be359d0
Syahrul050203 <syahrulfebriansah12@gmail.com>
2023-10-28 02:49:20 +0700

    feat : adding new code and organizing code - Created a ball detection function - Create a program to run motors and servos on the Arduino DC motor   control shield

--- Projek Robotika Meka 2021/src/main.cpp
+++ Projek Robotika Meka 2021/src/main.cpp
@@ -1,15 +1,42 @@
 #include "Arduino.h"
+#include "Servo.h"
+#include "AFMotor.h"
+
+#define SERVO1 10
+#define SERVO2 9
+
+const unsigned char speed_motor1 = 255;
+const unsigned char speed_motor2 = 255;
+
+Servo servo1;
+
+AF_DCMotor motor1(1, MOTOR12_64KHZ);
+AF_DCMotor motor2(2, MOTOR12_64KHZ);
 
 float data_analog;
 
 void setup() {
   Serial.begin(57600);
+
+  servo1.attach(SERVO1);
+  
+  motor1.setSpeed(speed_motor1);
+  motor2.setSpeed(speed_motor2);
 }
 
 void loop() { 
   detectionBall();
-  
-  delay(500);
+
+  servo1.write(0);
+  delay(1000);
+  servo1.write(180);
+  delay(1000);
+
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  delay(5000);
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
 }
 
 void detectionBall() {
